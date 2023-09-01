33115cc37cbd205f1989fc11a220385cc0238616
Niklas Kurzenberger <n.kurzenberger@MacBook-Pro-von-Administrator.local>
2023-09-01 15:41:10 +0200

    added react-leaflet and ios,android folders

--- karteVonMorgen/src/pages/Tab2.tsx
+++ karteVonMorgen/src/pages/Tab2.tsx
@@ -1,8 +1,10 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
 
-const Tab2: React.FC = () => {
+
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
@@ -16,10 +18,10 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+export default Tab1;
