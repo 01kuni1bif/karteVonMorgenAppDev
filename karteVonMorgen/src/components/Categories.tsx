9493fbafd8ca365d463193c27b936dea9678f0a1
01kuni1bif <niklas.kurzenberger@googlemail.com>
2023-10-09 17:20:20 +0200

    changed css for the categories

--- karteVonMorgen/src/components/Categories.tsx
+++ karteVonMorgen/src/components/Categories.tsx
@@ -18,19 +18,19 @@ const Categories: React.FC<{ onCategoryChange: (categories: string[]) => void }>
 
   return (
     <div className='categories'>
-      <IonGrid className="ion-align-items-center">
-        <IonRow className="ion-justify-content-center">
-          <IonCol size="12" sizeMd="4">
+      <IonGrid>
+        <IonRow>
+          <IonCol size="4" >
             <IonButton expand="block" className="custom-button" onClick={() => handleButtonClick('2cd00bebec0c48ba9db761da48678134')} color={selectedCategories.includes('2cd00bebec0c48ba9db761da48678134') ? 'primary' : 'light'}>
               initiatives
             </IonButton>
           </IonCol>
-          <IonCol size="12" sizeMd="4">
+          <IonCol size="4" >
             <IonButton expand="block" className="custom-button" onClick={() => handleButtonClick('77b3c33a92554bcf8e8c2c86cedd6f6f')} color={selectedCategories.includes('77b3c33a92554bcf8e8c2c86cedd6f6f') ? 'primary' : 'light'}>
               companies
             </IonButton>
           </IonCol>
-          <IonCol size="12" sizeMd="4">
+          <IonCol size="4" >
             <IonButton expand="block" className="custom-button" onClick={() => handleButtonClick('events')} color={selectedCategories.includes('events') ? 'primary' : 'light'}>
               events
             </IonButton>
