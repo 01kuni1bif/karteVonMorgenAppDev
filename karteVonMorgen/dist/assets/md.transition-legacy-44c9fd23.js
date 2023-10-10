f7eefb994eca50bc2282e4225eef40bcecfa458c
Jonas Kong <jonas_kong@outlook.com>
2023-10-10 16:41:13 +0200

    build sync

--- karteVonMorgen/dist/assets/md.transition-legacy-44c9fd23.js
+++ karteVonMorgen/dist/assets/md.transition-legacy-44c9fd23.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-c359e7d2.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-277e29dc.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- karteVonMorgen/dist/assets/swipe-back-legacy-4ed9ce6c.js
+++ karteVonMorgen/dist/assets/swipe-back-legacy-4ed9ce6c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-c359e7d2.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-277e29dc.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
